c198d37a878f84a4c67cfaf924e2533bcc80e919
Yifei <directoryyf@gmail.com>
2024-12-22 15:47:34 +0800

    feat: improvements

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -184,13 +184,13 @@ const CarouselItem = React.forwardRef<
       role="group"
       aria-roledescription="slide"
       className={cn(
-        "min-w-0 shrink-0 grow-0 basis-full",
+        "min-w-0 shrink-0 grow-0 basis-full flex items-center justify-center",
         orientation === "horizontal" ? "pl-4" : "pt-4",
         className
       )}
       {...props}
     />
-  )
+  );
 })
 CarouselItem.displayName = "CarouselItem"
 
