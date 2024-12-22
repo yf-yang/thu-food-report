bea6699c9ce405240ff8e245bff2a292561b3288
Yifei <directoryyf@gmail.com>
2024-12-22 20:09:02 +0800

    feat: improve padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import EntryForm from "@/components/entry-form";
 import Instruction from "@/markdown/serviceHallInstruction.mdx";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-8">
       <h1 className="text-3xl font-bold mb-2">食在华子</h1>
       <h3 className="text-xl font-bold mb-2">2024华子食堂消费报告</h3>
       <EntryForm />
